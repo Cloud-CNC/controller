9d6838586cf2dd845a2e3ebd36d36982d5eefa6c
Wakeful-Cloud <wakefulcloud@gmail.com>
2020-01-20 15:25:17 -0700

    Bug fix 1 + Serialport functionality

--- index.js
+++ index.js
@@ -4,9 +4,8 @@
 
 //Imports
 const config = require('./config.js');
-const fs = require('fs').promises;
+const fs = require('fs');
 const logger = require('./lib/logger.js');
-const package = require('./package.json');
 const serial = require('./lib/serial.js');
 const websocket = require('./lib/websocket.js');
 
@@ -14,30 +13,32 @@ const websocket = require('./lib/websocket.js');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 //Serial controller (Only for enabled machines)
-const serialController = new serial(config.machines.filter(machine => machine.enabled), logger);
+const serialController = new serial(config.machines.filter(machine => machine.enabled), config.controller.serialDelay, logger);
 
 //Websocket controller
-const websocketController = new websocket(config.core.url, config.crypto.key, config.meta.delay, logger);
-websocketController.on('open', () =>
-{
-  websocketController.bind(config.meta._id, package.version);
-});
+const websocketController = new websocket(config.core.url, config.controller._id, fs.readFileSync(config.controller.key, 'utf8'), config.controller.websocketDelay, logger);
 
 //Command event
 websocketController.on('command', (data, response) =>
 {
-  console.log(`Received command: ${data.command} destined to: ${data.machine}`);
-  response(`Received command: ${data.command} destined to: ${data.machine}`);
-
-  //serialController.send(data.machine, data.command);
+  //Send
+  serialController.send(data.machine, data.command).then(res =>
+  {
+    //Response
+    response(res);
+  });
 });
 
 //Execute event
-websocketController.on('execute', async (data, success) =>
+websocketController.on('execute', (data, success) =>
 {
-  console.log(`Received file destined to: ${data.machine}`);
-  await fs.writeFile('./file.gcode', data.file);
-  success(true);
+  //Add M28 + M29 (SD Card Commands)
+  data.file = `M28\n${data.file}M29\n`;
 
-  //serialController.send(data.machine, data.command);
+  //Send
+  serialController.send(data.machine, data.file).then(res =>
+  {    
+    //Response
+    success(/echo:Now fresh file/.test(res));
+  });
 });
